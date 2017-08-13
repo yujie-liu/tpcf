3e51267fbb584c3b3841bec47c0b17aa8fd81a15
Tri Nguyen <tnguy51@u.rochester.edu>
2017-08-13 12:56:41 -0400

    Add README. Add more comment into main.py. Add guideline to configuration file.

--- python/kdtree/1.11/main.py
+++ python/kdtree/1.11/main.py
@@ -1,4 +1,5 @@
-''' Main to run correlation_function.'''
+""" Sample script to run and plot two-point correlation function using
+module correlation_function """
 
 import numpy
 import matplotlib
@@ -7,59 +8,66 @@ from correlation_function import CorrelationFunction
 
 matplotlib.rc('font', size=15)
 
-def main():
-    ''' Main '''
-    tpcf = CorrelationFunction('config.cfg')
 
-    # Calculating normalized factor
+def main():
+    """ Run and plot two-point correlation function using
+    class correlation_function.CorrelationFunction """
+
+    # Constructor takes in configuration file names
+    tpcf = CorrelationFunction('sample_config.cfg')
+
+    # Get normalization factor (both weighted and unweighted)
     norm = numpy.array([tpcf.normalization(weighted=True),
                         tpcf.normalization(weighted=False)])
-    print("Normalization Factor:")
-    print("+ Weighted", norm[0])
-    print("+ Unweighted", norm[1])
 
-    # Construct RR(s)
+    # Construct separation distribution RR(s) between pairs of randoms
     rand_rand, bins_s = tpcf.rand_rand()
-    rand_rand[0] = rand_rand[0]/norm[0][0]
-    rand_rand[1] = rand_rand[1]/norm[1][0]
+    rand_rand[0] /= norm[0][0]  # normalize weighted RR(s)
+    rand_rand[1] /= norm[1][0]  # normalize unweighted RR(s)
 
-    # Construct DR(s)
-    data_rand, bins_s = tpcf.data_rand()
-    data_rand[0] = data_rand[0]/norm[0][1]
-    data_rand[1] = data_rand[1]/norm[1][1]
+    # Construct separation distribution DR(s) between pairs of a random point
+    # and a galaxies
+    data_rand, _ = tpcf.data_rand()
+    data_rand[0] /= norm[0][1]  # normalize weighted DR(s)
+    data_rand[1] /= norm[1][1]  # normalize unweighted DR(s)
 
-    # Construct DD(s)
-    data_data, bins_s = tpcf.data_data()
-    data_data[0] = data_data[0]/norm[0][2]
-    data_data[1] = data_data[1]/norm[1][2]
+    # Construct separation distribution DD(s) between pairs of galaxies
+    data_data, _ = tpcf.data_data()
+    data_data[0] /= norm[0][2]  # normalize weighted DD(s)
+    data_data[1] /= norm[1][2]  # normalize unweighted DD(s)
 
-    # Construct two-point correlation function
+    # Construct two-point correlation function, both weighted and unweighted
     correlation = [tpcf.correlation(rand_rand[i], data_rand[i], data_data[i],
                                     bins_s) for i in range(2)]
 
-    # Save histogram
-    numpy.savez("out/tpcf_North", RR=rand_rand, DR=data_rand, DD=data_data,
+    # Save RR(s), DR(s), DD(s) and tpcf into .npz format
+    numpy.savez("out/tpcf_sample",
+                RR=rand_rand, DR=data_rand, DD=data_data,
                 TPCF=correlation[0], TPCFSS=correlation[1],
                 BINS=bins_s)
 
-    # Save plot
+    # Create plot figure for RR(s), DR(s), DD(s) and tpcf
     figure, axes = matplotlib.pyplot.subplots(2, 2, figsize=(12, 8), sharex=True)
-    centers_s = 0.5*(bins_s[:-1]+bins_s[1:])
-    axes[0, 0].plot(centers_s, data_data[0], label='DD(s)')
-    axes[0, 0].plot(centers_s, data_rand[0], label='DR(s)')
-    axes[0, 0].plot(centers_s, rand_rand[0], label='RR(s)')
-    axes[0, 1].plot(centers_s, data_data[1], label='DD(s)')
-    axes[0, 1].plot(centers_s, data_rand[1], label='DR(s)')
-    axes[0, 1].plot(centers_s, rand_rand[1], label='DD(s)')
-    axes[1, 0].plot(centers_s, correlation[0][0], label='Weighted')
-    axes[1, 0].plot(centers_s, correlation[1][0], label='Unweighted')
-    axes[1, 1].plot(centers_s, correlation[0][1], label='Weighted')
-    axes[1, 1].plot(centers_s, correlation[1][1], label='Unweighted')
 
-    axes[0, 0].set(ylabel='Normed Counts',
-                   title='Weighted')
-    axes[0, 1].set(ylabel='Normed Counts',
-                   title='Unweighted')
+    bins_center = 0.5*(bins_s[:-1]+bins_s[1:])
+    # Plot weighted RR(s), DR(s), DD(s)
+    axes[0, 0].plot(bins_center, data_data[0], label='DD(s)')
+    axes[0, 0].plot(bins_center, data_rand[0], label='DR(s)')
+    axes[0, 0].plot(bins_center, rand_rand[0], label='RR(s)')
+    # Plot unweighted RR(s), DR(s), DD(s)
+    axes[0, 1].plot(bins_center, data_data[1], label='DD(s)')
+    axes[0, 1].plot(bins_center, data_rand[1], label='DR(s)')
+    axes[0, 1].plot(bins_center, rand_rand[1], label='DD(s)')
+    # Plot weighted and unweighted tpcf
+    axes[1, 0].plot(bins_center, correlation[0][0], label='Weighted')
+    axes[1, 0].plot(bins_center, correlation[1][0], label='Unweighted')
+    # Plot weighted and unweighted tpcfs^2
+    axes[1, 1].plot(bins_center, correlation[0][1], label='Weighted')
+    axes[1, 1].plot(bins_center, correlation[1][1], label='Unweighted')
+
+    # Set axis labels
+    axes[0, 0].set(ylabel='Normed Counts', title='Weighted')
+    axes[0, 1].set(ylabel='Normed Counts', title='Unweighted')
     axes[1, 0].set(ylabel=r'$\xi(s)$')
     axes[1, 1].set(ylabel=r'$\xi(s)s^{2}$')
     for ax in axes.flat:
@@ -67,9 +75,8 @@ def main():
         ax.legend()
 
     figure.tight_layout()
-
-
-    matplotlib.pyplot.savefig("North.png", bbox_inches='tight')
+    # Save figure
+    matplotlib.pyplot.savefig("sample.png", bbox_inches='tight')
 
 
 if __name__ == "__main__":
