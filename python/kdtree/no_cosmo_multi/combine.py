28f518da6756bc6b319ae275b521cd97b51e7cd7
Tri Nguyen <tnguy51@u.rochester.edu>
2017-08-22 19:44:32 -0400

    Delete test scripts. Add script for job submission and combining job.

---
+++ python/kdtree/no_cosmo_multi/combine.py
@@ -0,0 +1,60 @@
+""" Script for combining job results and calculate DD(s), DR(s), and RR(s) """
+
+import sys
+import glob
+import numpy
+from correlation_function import CorrelationFunction
+
+def main():
+    """ Main """
+    # Cmd argument are configurationfile and output prefix
+    config_fname = sys.argv[1]
+    prefix = sys.argv[2]
+
+    # Combining histogram by simply taking the sum in each bin
+    fname_list = glob.glob("out/{}*".format(prefix))
+    for i, fname in enumerate(fname_list):
+        print("Reading from {}".format(fname))
+        temp_file = numpy.load(fname)
+        if i is 0:
+            data_data = temp_file["DD"]
+            theta_hist = temp_file["ANGULAR_D"]
+            r_theta_hist = temp_file["ANGULAR_R"]
+        else:
+            data_data += temp_file["DD"]
+            theta_hist += temp_file["ANGULAR_D"]
+            r_theta_hist += temp_file["ANGULAR_R"]
+
+    # Create an instance of two-point correlation function that reads in
+    # configuration file
+    # IMPORTANT: setting should be the same with job config file.
+    tpcf = CorrelationFunction(config_fname)
+
+    # Calculate RR(s) and DR(s)
+    rand_rand, bins_s = tpcf.rand_rand(theta_hist)
+    data_rand, _ = tpcf.data_rand(r_theta_hist)
+
+    # Normalization
+    norm = numpy.array([tpcf.normalization(weighted=True),
+                        tpcf.normalization(weighted=False)])
+    for i in range(2):
+        rand_rand[i] = rand_rand[i]/norm[i][0]
+        data_rand[i] = data_rand[i]/norm[i][1]
+        data_data[i] = data_data[i]/norm[i][2]
+
+    # Construct two-point correlation function, both weighted and unweighted
+    correlation = numpy.zeros((2, 2, bins_s.size-1))
+    correlation[0] = tpcf.correlation(rand_rand[0], data_rand[0], data_data[0],
+                                      bins_s)
+    correlation[1] = tpcf.correlation(rand_rand[1], data_rand[1], data_data[1],
+                                      bins_s)
+
+    # Save results
+    numpy.savez("out/{}_final".format(prefix),
+                DD=data_data, RR=rand_rand, DR=data_rand,
+                TPCF=correlation[:, 0], TPCFSS=correlation[:, 1],
+                BINS=bins_s)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/kdtree/no_cosmo_multi/divide.py
@@ -0,0 +1,39 @@
+""" Script for submitting jobs to calculate DD(s), DR(s), and RR(s) """
+
+import sys
+import numpy
+from correlation_function import CorrelationFunction
+
+def main():
+    """ Main """
+
+    # Cmd arguments are job number, total number of jobs, configuration file,
+    # and output prefix
+    # Job number must be a nonnegative integer less than total number of jobs
+    # Total number of jobs must be at least 1
+    no_job = int(sys.argv[1])
+    total_jobs = int(sys.argv[2])
+    config_fname = sys.argv[3]
+    prefix = sys.argv[4]
+
+    # Create an instance of two-point correlation function that reads in
+    # configuration file
+    tpcf = CorrelationFunction(config_fname)
+
+    # Calculate child-process histogram
+    print("Job number: {}. Total jobs: {}.".format(no_job, total_jobs))
+    # Angular distance distribution f(theta)
+    theta_hist, _ = tpcf.angular_distance(no_job, total_jobs)
+    # Radial angular distribution g(theta, r)
+    r_theta_hist, _, _ = tpcf.r_angular_distance(no_job, total_jobs)
+    # Galaxies separation distribution DD(s)
+    data_data, _ = tpcf.data_data(no_job, total_jobs)
+
+    # Save with prefix
+    numpy.savez("out/{}_{:03d}".format(prefix, no_job),
+                DD=data_data, ANGULAR_D=theta_hist, ANGULAR_R=r_theta_hist)
+
+
+
+if __name__ == "__main__":
+    main()
