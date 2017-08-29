d6f88785d2b7b8a8ad1089890ac9f50231a5cd3c
Tri Nguyen <tnguy51@u.rochester.edu>
2017-08-29 14:41:36 -0400

    Same as last one

--- python/no_cosmo/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# divide into multiple job - no dependencies
-jid1=$(sbatch batch_divide.sh)
-
-# combine into one - depend on first job to be successfully completed
-jid2=$(sbatch --dependency=singleton batch_combine.sh)
-
